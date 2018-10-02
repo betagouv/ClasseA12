dd8ced948fc10ef7f8e8a59b958f613450e28ac3
Mathieu Agopian <mathieu@agopian.info>
2018-10-02 15:15:24 +0200

    Display the videos as a list of cards

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -41,11 +41,41 @@ update _ msg model =
 view : Session -> Model -> ( String, List (H.Html Msg) )
 view _ model =
     ( "Liste des vidéos"
-    , model.videoList
-        |> List.map
-            (\video ->
-                H.a [ HA.href "#" ]
-                    [ H.img [ HA.src video.thumbnail ] []
-                    ]
+    , [ H.div
+            [ HA.class "box" ]
+            [ H.div [ HA.class "field has-addons" ]
+                [ H.div [ HA.class "control is-expanded" ]
+                    [ H.input [ HA.class "input", HA.type_ "search", HA.placeholder "Search video titles" ] [] ]
+                , H.div [ HA.class "control" ]
+                    [ H.a [ HA.class "button is-info" ] [ H.text "Search" ] ]
+                ]
+            ]
+      , H.div [ HA.class "row columns is-multiline" ]
+            (model.videoList
+                |> List.map
+                    (\video ->
+                        H.div [ HA.class "column is-one-quarter" ]
+                            [ H.a [ HA.href video.url ]
+                                [ H.div [ HA.class "card large round" ]
+                                    [ H.div [ HA.class "card-image " ]
+                                        [ H.figure [ HA.class "image" ]
+                                            [ H.img
+                                                [ HA.src video.thumbnail
+                                                , HA.alt <| "Thumbnail of the video titled: " ++ video.title
+                                                ]
+                                                []
+                                            ]
+                                        ]
+                                    , H.div [ HA.class "card-content" ]
+                                        [ H.div [ HA.class "content" ]
+                                            [ H.text video.author
+                                            , H.p [ HA.class "video-date" ] [ H.text video.date ]
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                    )
             )
+      ]
     )
