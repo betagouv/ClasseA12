d239319c2f17247b9ce74654f0a8c884e8db17ab
Mathieu Agopian <mathieu@agopian.info>
2018-10-09 11:33:41 +0200

    Search video titles

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -3,25 +3,28 @@ module Page.Home exposing (Model, Msg(..), init, update, view)
 import Data.Session exposing (Session, VideoData(..))
 import Html as H
 import Html.Attributes as HA
+import Html.Events as HE
 import Http
 
 
 type alias Model =
-    {}
+    { search : String }
 
 
 type Msg
-    = Noop
+    = UpdateSearch String
 
 
 init : Session -> ( Model, Cmd Msg )
 init session =
-    ( {}, Cmd.none )
+    ( { search = "" }, Cmd.none )
 
 
 update : Session -> Msg -> Model -> ( Model, Cmd Msg )
 update _ msg model =
-    ( model, Cmd.none )
+    case msg of
+        UpdateSearch newSearch ->
+            ( { model | search = newSearch }, Cmd.none )
 
 
 view : Session -> Model -> ( String, List (H.Html Msg) )
@@ -32,26 +35,39 @@ view session model =
             [ H.text "Chargement des vidéos..." ]
 
         Received videoList ->
-            viewVideoList videoList
+            viewVideoList model.search videoList
 
         Error error ->
             [ H.text <| "Erreur lors du chargement des videos: " ++ error ]
     )
 
 
-viewVideoList : List Data.Session.Video -> List (H.Html Msg)
-viewVideoList videoList =
+viewVideoList : String -> List Data.Session.Video -> List (H.Html Msg)
+viewVideoList search videoList =
+    let
+        filteredVideoList =
+            videoList
+                |> List.filter (\video -> String.contains search video.title)
+    in
     [ H.div
         [ HA.class "box" ]
         [ H.div [ HA.class "field has-addons" ]
             [ H.div [ HA.class "control is-expanded" ]
-                [ H.input [ HA.class "input", HA.type_ "search", HA.placeholder "Search video titles" ] [] ]
+                [ H.input
+                    [ HA.class "input"
+                    , HA.type_ "search"
+                    , HA.placeholder "Search video titles"
+                    , HA.value search
+                    , HE.onInput UpdateSearch
+                    ]
+                    []
+                ]
             , H.div [ HA.class "control" ]
                 [ H.a [ HA.class "button is-info" ] [ H.text "Search" ] ]
             ]
         ]
     , H.div [ HA.class "row columns is-multiline" ]
-        (videoList
+        (filteredVideoList
             |> List.map
                 (\video ->
                     H.div [ HA.class "column is-one-quarter" ]
