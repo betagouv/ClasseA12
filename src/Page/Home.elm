130d20e0c89530d037fa1f6140a5c454bf696c94
Mathieu Agopian <mathieu@agopian.info>
2018-10-09 16:44:13 +0200

    Display the video player in an iframe in a modal

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -5,19 +5,24 @@ import Html as H
 import Html.Attributes as HA
 import Html.Events as HE
 import Http
+import Json.Encode as Encode
 
 
 type alias Model =
-    { search : String }
+    { search : String
+    , activeVideo : Maybe Data.Session.Video
+    }
 
 
 type Msg
     = UpdateSearch String
+    | ShowVideo Data.Session.Video
+    | HideVideo
 
 
 init : Session -> ( Model, Cmd Msg )
 init session =
-    ( { search = "" }, Cmd.none )
+    ( { search = "", activeVideo = Nothing }, Cmd.none )
 
 
 update : Session -> Msg -> Model -> ( Model, Cmd Msg )
@@ -25,6 +30,12 @@ update _ msg model =
     case msg of
         UpdateSearch newSearch ->
             ( { model | search = newSearch }, Cmd.none )
+
+        ShowVideo video ->
+            ( { model | activeVideo = Just video }, Cmd.none )
+
+        HideVideo ->
+            ( { model | activeVideo = Nothing }, Cmd.none )
 
 
 view : Session -> Model -> ( String, List (H.Html Msg) )
@@ -35,15 +46,15 @@ view session model =
             [ H.text "Chargement des vidéos..." ]
 
         Received videoList ->
-            viewVideoList model.search videoList
+            viewVideoList model videoList
 
         Error error ->
             [ H.text <| "Erreur lors du chargement des videos: " ++ error ]
     )
 
 
-viewVideoList : String -> List Data.Session.Video -> List (H.Html Msg)
-viewVideoList search videoList =
+viewVideoList : { a | activeVideo : Maybe Data.Session.Video, search : String } -> List Data.Session.Video -> List (H.Html Msg)
+viewVideoList ({ search } as model) videoList =
     let
         filteredVideoList =
             videoList
@@ -68,29 +79,65 @@ viewVideoList search videoList =
         ]
     , H.div [ HA.class "row columns is-multiline" ]
         (filteredVideoList
-            |> List.map
-                (\video ->
-                    H.div [ HA.class "column is-one-quarter" ]
-                        [ H.a [ HA.href video.link ]
-                            [ H.div [ HA.class "card large round" ]
-                                [ H.div [ HA.class "card-image " ]
-                                    [ H.figure [ HA.class "image" ]
-                                        [ H.img
-                                            [ HA.src video.thumbnail
-                                            , HA.alt <| "Thumbnail of the video titled: " ++ video.title
-                                            ]
-                                            []
-                                        ]
-                                    ]
-                                , H.div [ HA.class "card-content" ]
-                                    [ H.div [ HA.class "content" ]
-                                        [ H.text video.title
-                                        , H.p [ HA.class "video-date" ] [ H.text video.pubDate ]
-                                        ]
-                                    ]
-                                ]
-                            ]
-                        ]
-                )
+            |> List.map (viewVideo model)
         )
     ]
+
+
+viewVideo : { a | activeVideo : Maybe Data.Session.Video } -> Data.Session.Video -> H.Html Msg
+viewVideo { activeVideo } video =
+    let
+        active =
+            activeVideo
+                |> Maybe.map ((==) video)
+                |> Maybe.withDefault False
+    in
+    H.div [ HA.class "column is-one-quarter" ]
+        [ H.div
+            [ HA.classList
+                [ ( "modal", True )
+                , ( "is-active", active )
+                ]
+            , HE.onClick HideVideo
+            ]
+            [ H.div [ HA.class "modal-background" ] []
+            , H.div [ HA.class "modal-content" ]
+                [ H.iframe
+                    [ HA.src video.player
+                    , HA.title "Partie 2 : am&eacute;nager sa classe pour co-enseigner."
+                    , stringProperty "scrolling" "no"
+                    , stringProperty "frameborder" "0"
+                    , stringProperty "allowfullscreen" "true"
+                    ]
+                    []
+                ]
+            , H.button [ HA.class "modal-close is-large" ] []
+            ]
+        , H.a
+            [ HA.href "#"
+            , HE.onClick <| ShowVideo video
+            ]
+            [ H.div [ HA.class "card large round" ]
+                [ H.div [ HA.class "card-image " ]
+                    [ H.figure [ HA.class "image" ]
+                        [ H.img
+                            [ HA.src video.thumbnail
+                            , HA.alt <| "Thumbnail of the video titled: " ++ video.title
+                            ]
+                            []
+                        ]
+                    ]
+                , H.div [ HA.class "card-content" ]
+                    [ H.div [ HA.class "content" ]
+                        [ H.text video.title
+                        , H.p [ HA.class "video-date" ] [ H.text video.pubDate ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+
+
+stringProperty : String -> String -> H.Attribute msg
+stringProperty name value =
+    HA.property name <| Encode.string value
