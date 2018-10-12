e3ed7f06957e4809a0ed62bc014a43fa4fcba49d
Mathieu Agopian <mathieu@agopian.info>
2018-10-12 20:13:05 +0200

    Some more refactoring in Participate.elm

--- src/Page/Participate.elm
+++ src/Page/Participate.elm
@@ -1,7 +1,7 @@
 module Page.Participate exposing (Model, Msg(..), init, update, view)
 
 import Data.Kinto exposing (KintoData(..))
-import Data.Session exposing (Session)
+import Data.Session exposing (Session, Video)
 import Html as H
 import Html.Attributes as HA
 import Html.Events as HE
@@ -10,8 +10,8 @@ import Request.KintoUpcoming
 
 
 type alias Model =
-    { newVideo : Data.Session.Video
-    , newVideoKintoData : KintoData Data.Session.Video
+    { newVideo : Video
+    , newVideoKintoData : KintoData Video
     , error : Maybe String
     }
 
@@ -27,9 +27,9 @@ emptyVideo =
 
 
 type Msg
-    = UpdateVideoForm Data.Session.Video
+    = UpdateVideoForm Video
     | SubmitNewVideo
-    | NewVideoSubmitted (Result Kinto.Error Data.Session.Video)
+    | NewVideoSubmitted (Result Kinto.Error Video)
     | DiscardError
 
 
@@ -81,131 +81,48 @@ view _ { newVideo, newVideoKintoData, error } =
             [ displayError error
             , H.text "Vous aimeriez avoir l'avis de vos collègues sur une problématique ou souhaitez poster une vidéo pour aider le collectif, contactez-nous !"
             , H.form [ HE.onSubmit SubmitNewVideo ]
-                [ H.div [ HA.class "field is-horizontal" ]
-                    [ H.div [ HA.class "field-label is-normal" ]
-                        [ H.label [ HA.class "label", HA.for "title" ]
-                            [ H.text "Titre" ]
-                        ]
-                    , H.div [ HA.class "field-body" ]
-                        [ H.div [ HA.class "field" ]
-                            [ H.div [ HA.class "control" ]
-                                [ H.input
-                                    [ HA.class "input"
-                                    , HA.type_ "text"
-                                    , HA.id "title"
-                                    , HA.placeholder "Titre de la vidéo"
-                                    , HA.value newVideo.title
-                                    , HE.onInput <| \title -> UpdateVideoForm { newVideo | title = title }
-                                    ]
-                                    []
-                                ]
-                            ]
-                        ]
-                    ]
-                , H.div [ HA.class "field is-horizontal" ]
-                    [ H.div [ HA.class "field-label is-normal" ]
-                        [ H.label [ HA.class "label", HA.for "link" ]
-                            [ H.text "Lien vers la vidéo" ]
-                        ]
-                    , H.div [ HA.class "field-body" ]
-                        [ H.div [ HA.class "field" ]
-                            [ H.div [ HA.class "control" ]
-                                [ H.input
-                                    [ HA.class "input"
-                                    , HA.type_ "text"
-                                    , HA.id "link"
-                                    , HA.placeholder "https://example.com/mavideo"
-                                    , HA.value newVideo.link
-                                    , HE.onInput <| \link -> UpdateVideoForm { newVideo | link = link }
-                                    ]
-                                    []
-                                ]
-                            ]
-                        ]
-                    ]
-                , H.div [ HA.class "field is-horizontal" ]
-                    [ H.div [ HA.class "field-label is-normal" ]
-                        [ H.label [ HA.class "label", HA.for "thumbnail" ]
-                            [ H.text "Lien vers la miniature de la vidéo" ]
-                        ]
-                    , H.div [ HA.class "field-body" ]
-                        [ H.div [ HA.class "field" ]
-                            [ H.div [ HA.class "control" ]
-                                [ H.input
-                                    [ HA.class "input"
-                                    , HA.type_ "text"
-                                    , HA.id "thumbnail"
-                                    , HA.placeholder "https://example.com/mavideo.png"
-                                    , HA.value newVideo.thumbnail
-                                    , HE.onInput <| \thumbnail -> UpdateVideoForm { newVideo | thumbnail = thumbnail }
-                                    ]
-                                    []
-                                ]
-                            ]
-                        ]
-                    ]
-                , H.div [ HA.class "field is-horizontal" ]
-                    [ H.div [ HA.class "field-label is-normal" ]
-                        [ H.label [ HA.class "label", HA.for "pubDate" ]
-                            [ H.text "Date de la vidéo" ]
-                        ]
-                    , H.div [ HA.class "field-body" ]
-                        [ H.div [ HA.class "field" ]
-                            [ H.div [ HA.class "control" ]
-                                [ H.input
-                                    [ HA.class "input"
-                                    , HA.type_ "text"
-                                    , HA.id "pubDate"
-                                    , HA.placeholder "2018-10-12"
-                                    , HA.value newVideo.pubDate
-                                    , HE.onInput <| \pubDate -> UpdateVideoForm { newVideo | pubDate = pubDate }
-                                    ]
-                                    []
-                                ]
-                            ]
-                        ]
-                    ]
-                , H.div [ HA.class "field is-horizontal" ]
-                    [ H.div [ HA.class "field-label is-normal" ]
-                        [ H.label [ HA.class "label", HA.for "player" ]
-                            [ H.text "Lien vers le player de la vidéo" ]
-                        ]
-                    , H.div [ HA.class "field-body" ]
-                        [ H.div [ HA.class "field" ]
-                            [ H.div [ HA.class "control" ]
-                                [ H.input
-                                    [ HA.class "input"
-                                    , HA.type_ "text"
-                                    , HA.id "player"
-                                    , HA.placeholder "https://player.example.com/mavideo"
-                                    , HA.value newVideo.player
-                                    , HE.onInput <| \player -> UpdateVideoForm { newVideo | player = player }
-                                    ]
-                                    []
-                                ]
-                            ]
-                        ]
-                    ]
-                , H.div [ HA.class "field is-horizontal" ]
-                    [ H.div [ HA.class "field-label is-normal" ]
-                        [ H.label [ HA.class "label", HA.for "description" ]
-                            [ H.text "Description" ]
-                        ]
-                    , H.div [ HA.class "field-body" ]
-                        [ H.div [ HA.class "field" ]
-                            [ H.div [ HA.class "control" ]
-                                [ H.textarea
-                                    [ HA.class "textarea"
-                                    , HA.id "description"
-                                    , HA.placeholder "Description : que montre la vidéo ?"
-                                    , HA.value newVideo.description
-                                    , HE.onInput <| \description -> UpdateVideoForm { newVideo | description = description }
-                                    ]
-                                    []
-                                ]
-                            ]
-                        ]
-                    ]
+                [ formInput
+                    H.input
+                    "title"
+                    "Titre"
+                    "Titre de la video"
+                    newVideo.title
+                    (\title -> UpdateVideoForm { newVideo | title = title })
+                , formInput
+                    H.input
+                    "link"
+                    "Lien vers la vidéo"
+                    "https://example.com/mavideo"
+                    newVideo.link
+                    (\link -> UpdateVideoForm { newVideo | link = link })
+                , formInput
+                    H.input
+                    "thumbnail"
+                    "Lien vers la miniature de la vidéo"
+                    "https://example.com/mavideo.png"
+                    newVideo.thumbnail
+                    (\thumbnail -> UpdateVideoForm { newVideo | thumbnail = thumbnail })
+                , formInput
+                    H.input
+                    "pubDate"
+                    "Date de la vidéo"
+                    "2018-10-12"
+                    newVideo.pubDate
+                    (\pubDate -> UpdateVideoForm { newVideo | pubDate = pubDate })
+                , formInput
+                    H.input
+                    "player"
+                    "Lien vers le player de la vidéo"
+                    "https://player.example.com/mavideo"
+                    newVideo.player
+                    (\player -> UpdateVideoForm { newVideo | player = player })
+                , formInput
+                    H.textarea
+                    "description"
+                    "Description"
+                    "Description : que montre la vidéo ?"
+                    newVideo.description
+                    (\description -> UpdateVideoForm { newVideo | description = description })
                 , H.div [ HA.class "field is-horizontal" ]
                     [ H.div [ HA.class "control" ]
                         [ loadingButton "Soumettre cette vidéo" newVideoKintoData ]
@@ -233,7 +150,41 @@ displayError maybeError =
             H.div [] []
 
 
-loadingButton : String -> KintoData Data.Session.Video -> H.Html Msg
+type alias HtmlNode msg =
+    List (H.Attribute msg) -> List (H.Html msg) -> H.Html msg
+
+
+formInput : HtmlNode msg -> String -> String -> String -> String -> (String -> msg) -> H.Html msg
+formInput input id label placeholder value onInput =
+    H.div [ HA.class "field is-horizontal" ]
+        [ H.div [ HA.class "field-label is-normal" ]
+            [ H.label [ HA.class "label", HA.for id ]
+                [ H.text label ]
+            ]
+        , H.div [ HA.class "field-body" ]
+            [ H.div [ HA.class "field" ]
+                [ H.div [ HA.class "control" ]
+                    [ input
+                        [ HA.class <|
+                            -- UGLY : special casing the textarea class, this thing is very weird in the Bulma css framework.
+                            if id == "description" then
+                                "textarea"
+
+                            else
+                                "input"
+                        , HA.id id
+                        , HA.placeholder placeholder
+                        , HA.value value
+                        , HE.onInput onInput
+                        ]
+                        []
+                    ]
+                ]
+            ]
+        ]
+
+
+loadingButton : String -> KintoData Video -> H.Html Msg
 loadingButton label dataState =
     let
         loading =
