d3cfc6f9467078347d83077cbe519d2608524a3d
Mathieu Agopian <mathieu@agopian.info>
2018-10-12 16:37:12 +0200

    Add kinto dependency and decoders for upcoming videos

--- src/Data/Session.elm
+++ src/Data/Session.elm
@@ -1,6 +1,8 @@
 module Data.Session exposing (Session, Video, VideoData(..), decodeVideoList)
 
 import Json.Decode as Decode
+import Json.Encode as Encode
+import Kinto
 
 
 type alias Session =
@@ -39,3 +41,29 @@ decodeVideoList : Decode.Value -> Result Decode.Error (List Video)
 decodeVideoList =
     Decode.decodeValue <|
         Decode.list videoDecoder
+
+
+
+---- KINTO ----
+
+
+encodeData : String -> String -> String -> String -> String -> String -> Encode.Value
+encodeData description link player pubDate thumbnail title =
+    Encode.object
+        [ ( "description", Encode.string description )
+        , ( "link", Encode.string link )
+        , ( "player", Encode.string player )
+        , ( "pubDate", Encode.string pubDate )
+        , ( "thumbnail", Encode.string thumbnail )
+        , ( "title", Encode.string title )
+        ]
+
+
+recordResource : Kinto.Resource Video
+recordResource =
+    Kinto.recordResource "classea12" "upcoming" videoDecoder
+
+
+upcomingVideosClient : Kinto.Client
+upcomingVideosClient =
+    Kinto.client "https://kinto.agopian.info/v1/" (Kinto.Basic "classea12" "notasecret")
