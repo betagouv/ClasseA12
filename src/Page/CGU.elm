a0a6c461445b8efd9528fd6e68e1ae4d91f011ac
Mathieu Agopian <mathieu@agopian.info>
2018-11-20 13:51:17 +0100

    Update the 'charte de bonne conduite'

--- src/Page/CGU.elm
+++ src/Page/CGU.elm
@@ -42,13 +42,10 @@ view _ _ =
                         [ H.text "Définition du projet :" ]
                     , H.ul []
                         [ H.li []
-                            [ H.text "Ce service est un réseau social à base de vidéos de pratiques pédagogiques entre enseignants de classes dédoublées (CP et CE1). Il a pour objectif de faciliter les échanges entre pairs et de mutualiste les pratiques." ]
+                            [ H.text "Ce service est un réseau social à base de vidéos de pratiques pédagogiques entre enseignants de classes dédoublées (CP et CE1). Il a pour objectif de faciliter les échanges entre pairs et de mutualiser les pratiques." ]
                         , H.li
                             []
-                            [ H.text "L’esprit Classe à 12 :  Un espace commun : tous les usagers et partenaires de Classe à 12 sont à la fois dépositaires et bénéficiaires des ressources et services proposés." ]
-                        , H.li
-                            []
-                            [ H.text "L’esprit Classe à 12 : Un écosystème apprenant : les acteurs qui gravitent autour de Classe à 12 s’enrichissent mutuellement et nourrissent la plateforme de leurs productions." ]
+                            [ H.text "Le site «\u{00A0}Classe à 12\u{00A0}» est un espace commun : tous les usagers et partenaires de Classe à 12 sont à la fois dépositaires et bénéficiaires des ressources et services proposés. C’est  un écosystème apprenant : les acteurs qui gravitent autour de Classe à 12\u{00A0}s’enrichissent mutuellement et nourrissent la plateforme de leurs productions." ]
                         , H.li
                             []
                             [ H.text "Le site Classe à 12 : propose un cadre neutre d’échanges horizontaux : les échanges se font dans un esprit d’ouverture et de bienveillance entre pairs." ]
@@ -57,10 +54,12 @@ view _ _ =
                         [ H.text "Conditions d’utilisation :" ]
                     , H.ul []
                         [ H.li []
+                            [ H.text "Les droits d’utilisation nécessitent la validation préalable des conditions générales d’utilisation figurant sur le site." ]
+                        , H.li []
                             [ H.text "Le site n’a pas pour vocation de contrôler ni de valider les productions." ]
                         , H.li
                             []
-                            [ H.text "Les vidéos non conformes aux programmes ou à une éthique professionnelle feront l’objet d’une non publication ou dépublication." ]
+                            [ H.text "Les vidéos non conformes aux programmes ou à une éthique professionnelle feront l’objet d’une non publication ou dé-publication." ]
                         , H.li
                             []
                             [ H.text "Vous trouverez ci-dessous, les points importants à respecter pour garantir la qualité et le bon fonctionnement des échanges. En cas de non respect des points suivants, vous engagez votre entière responsabilité." ]
@@ -72,28 +71,13 @@ view _ _ =
                             [ H.text "L’enseignant aura fait signer les documents nécessaires à l’autorisation de publication aux personnes apparaissant sur les vidéos publiés." ]
                         , H.li
                             []
-                            [ H.text "Seuls les personnels de l’éducation nationale peuvent créer un compte utilisateur avec leur adresse mail académique. Seuls les personnes inscrites peuvent publier des vidéos et profiter des fonctionnalités de réseautage." ]
+                            [ H.text "Seuls les personnels de l’éducation nationale peuvent créer un compte utilisateur avec leur adresse mail académique. Seules les personnes inscrites peuvent publier des vidéos et profiter des fonctionnalités de réseautage." ]
                         , H.li
                             []
                             [ H.text "Nous vous invitons à veiller à ne pas utiliser de termes ou expressions susceptibles de choquer les autres Utilisateurs, d'être perçus comme provocants ou obscènes, attentatoires aux bonnes mœurs, diffamatoires, insultants, discriminatoires ou incitant à la haine raciale, religieuse, politique ou autre, que ces termes ou expressions soient proscrits par la loi ou simplement contraires au respect et à la dignité de la personne humaine." ]
                         , H.li
                             []
-                            [ H.text "Si vous avez le sentiment ou si vous constatez qu'un Utilisateur ne respecte pas les conditions énoncée dans la présente charte, auxquelles il a pourtant souscrit lors de son inscription, ou que son comportement est répréhensible au regard de la loi ou de la morale, vous devez le signaler au modérateur du Site Internet en utilisant le formulaire de contact. Le modérateur pourra, le cas échéant, adresser un avertissement à l'utilisateur en cause et/ou arrêter sa connexion Membre." ]
-                        ]
-                    , H.p []
-                        [ H.text "Conditions réglementaires du site :" ]
-                    , H.ul []
-                        [ H.li []
-                            [ H.text "hébergement et statut" ]
-                        , H.li
-                            []
-                            [ H.text "Références RGPD" ]
-                        , H.li
-                            []
-                            [ H.text "Auteurs et responsables" ]
-                        , H.li
-                            []
-                            [ H.text "Autres...." ]
+                            [ H.text "Si vous avez le sentiment ou si vous constatez qu'un Utilisateur ne respecte pas les conditions énoncées dans la présente charte, auxquelles il a pourtant souscrit lors de son inscription, ou que son comportement est répréhensible au regard de la loi ou de la morale, vous devez le signaler au modérateur du Site Internet en utilisant le formulaire de contact. Le modérateur pourra, le cas échéant, adresser un avertissement à l'utilisateur en cause et/ou arrêter sa connexion Membre." ]
                         ]
                     , H.p []
                         [ H.text "Références juridiques utilisées pour la rédaction de la présente charte :" ]
