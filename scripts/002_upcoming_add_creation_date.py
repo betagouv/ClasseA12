5c0d9db8b9670203e6a0088658d6ffd1ffafe931
Mathieu Agopian <mathieu@agopian.info>
2018-11-30 16:26:57 +0100

    Add migration scripts to add creation_date

---
+++ scripts/002_upcoming_add_creation_date.py
@@ -0,0 +1,72 @@
+"""Migrate the `upcoming` records to have a creation date.
+
+For each `upcoming` record, copy the `last_modified` date as the `creation_date`.
+
+Requirements: kinto_http
+
+$ pip install kinto_http
+
+To use: run the following
+
+$ python 002_upcoming_add_creation_date.py --auth "<admin login>:<admin password>"
+
+"""
+
+import base64
+import json
+import os
+import pprint
+import urllib.request
+import time
+import datetime
+
+from kinto_http import cli_utils
+from kinto_http.exceptions import KintoException
+
+DEFAULT_SERVER = "https://kinto.agopian.info/v1/"
+DEFAULT_BUCKET = "classea12"
+DEFAULT_COLLECTION = "upcoming"
+
+
+def add_creation_date(videos):
+    for video in videos:
+        video["creation_date"] = video["last_modified"]
+
+
+def update_videos(client, videos):
+    for video in videos:
+        print(
+            "Updating video", video["id"], "with creation date", video["creation_date"]
+        )
+        client.update_record(data=video)
+
+
+def main():
+    parser = cli_utils.add_parser_options(
+        description="Add a creation_date field to the `upcoming` records",
+        default_server=DEFAULT_SERVER,
+        default_bucket=DEFAULT_BUCKET,
+        default_collection=DEFAULT_COLLECTION,
+    )
+
+    args = parser.parse_args()
+
+    client = cli_utils.create_client_from_args(args)
+
+    try:
+        client.create_bucket(if_not_exists=True)
+        client.create_collection(if_not_exists=True)
+    except KintoException:
+        # Fail silently in case of 403
+        pass
+
+    print("Fetching records")
+    videos = client.get_records()
+    print("Found", len(videos), "videos, use last_modified date for the creation_date")
+    add_creation_date(videos)
+    print("Updating records")
+    update_videos(client, videos)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/002_videos_add_creation_date.py
@@ -0,0 +1,89 @@
+"""Migrate the `videos` records to have a creation date.
+
+For each `videos` record, run a GET on the vimeo API page to retrieve the `upload_date`.
+If the GET returns with a 404, it means it's not a video downloaded from
+vimeo, in which case use the `last_modification` date.
+
+Requirements: kinto_http
+
+$ pip install kinto_http
+
+To use: run the following
+
+$ python 002_videos_add_creation_date.py --auth "<admin login>:<admin password>"
+
+"""
+
+import base64
+import json
+import os
+import pprint
+import urllib.request
+import time
+import datetime
+
+from kinto_http import cli_utils
+from kinto_http.exceptions import KintoException
+
+DEFAULT_SERVER = "https://kinto.agopian.info/v1/"
+DEFAULT_BUCKET = "classea12"
+DEFAULT_COLLECTION = "videos"
+
+
+def add_creation_date(videos):
+    for video in videos:
+        video_filename = video["attachment"]["filename"]
+        vimeo_id = video_filename.replace(".mp4", "")
+        vimeo_data_url = f"https://vimeo.com/api/v2/video/{vimeo_id}.json"
+        try:
+            vimeo_data = urllib.request.urlopen(vimeo_data_url)
+        except urllib.error.HTTPError:
+            print("Error while retrieving vimeo data at", vimeo_data_url)
+            continue
+
+        vimeo_json = json.loads(vimeo_data.read())[0]
+        creation_date_str = vimeo_json["upload_date"]
+        day = datetime.datetime.strptime(creation_date_str, "%Y-%m-%d %H:%M:%S")
+        creation_date_timestamp = round(
+            time.mktime(day.timetuple()) * 1000
+        )  # in milliseconds
+        video["creation_date"] = creation_date_timestamp
+
+
+def update_videos(client, videos):
+    for video in videos:
+        print(
+            "Updating video", video["id"], "with creation date", video["creation_date"]
+        )
+        client.update_record(data=video)
+
+
+def main():
+    parser = cli_utils.add_parser_options(
+        description="Add a creation_date field to the `videos` records",
+        default_server=DEFAULT_SERVER,
+        default_bucket=DEFAULT_BUCKET,
+        default_collection=DEFAULT_COLLECTION,
+    )
+
+    args = parser.parse_args()
+
+    client = cli_utils.create_client_from_args(args)
+
+    try:
+        client.create_bucket(if_not_exists=True)
+        client.create_collection(if_not_exists=True)
+    except KintoException:
+        # Fail silently in case of 403
+        pass
+
+    print("Fetching records")
+    videos = client.get_records()
+    print("Found", len(videos), "videos, get creation dates from vimeo")
+    add_creation_date(videos)
+    print("Updating records")
+    update_videos(client, videos)
+
+
+if __name__ == "__main__":
+    main()
