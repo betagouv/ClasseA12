1f4c301a37f36713fc4ecdcadfbe2e9d0833a9c6
Mathieu Agopian <mathieu@agopian.info>
2018-12-06 16:57:37 +0100

    Move the thumbnails to be stored on disk instead as data URL in records fields

---
+++ scripts/004_videos_move_dataurl_to_thumbnails.py
@@ -0,0 +1,117 @@
+"""Migrate the thumbnails from the `videos` records to the thumbnails collection.
+
+For each `videos` record, store the thumbnail data (stored as a data URL in
+the database) as an attachment on a record created in the `thumbnails`
+collection.
+
+Requirements: kinto_http
+
+$ pip install kinto_http
+
+To use: run the following
+
+$ python 004_videos_move_dataurl_to_thumbnails.py --auth "<admin login>:<admin password>"
+
+"""
+
+import base64
+import json
+import os
+import pprint
+import urllib.request
+import time
+import datetime
+import urllib.request
+import uuid
+import mimetypes
+
+from kinto_http import cli_utils
+from kinto_http.exceptions import KintoException
+
+import collections_metadata
+
+DEFAULT_SERVER = "https://kinto.agopian.info/v1/"
+DEFAULT_BUCKET = "classea12"
+DEFAULT_COLLECTION = "videos"
+
+
+def upload_thumbnails(client, videos):
+    for video in videos:
+        # Save the thumbnail data URL as a real file on disk.
+        filename, headers = urllib.request.urlretrieve(video["thumbnail"])
+        content_type = headers.get_content_type()
+        # Upload the thumbnail as an attachment.
+        extension = mimetypes.guess_extension(content_type)
+        files = [
+            (
+                "attachment",
+                (f"{filename}{extension}", open(filename, "rb"), content_type),
+            )
+        ]
+
+        data = {"for": video["id"]}
+        record_id = str(uuid.uuid4())
+        record_uri = client.get_endpoint("record", id=record_id)
+        attachment_uri = "%s/attachment" % record_uri
+        # The client points to the "videos" collection but we want to upload to the thumbnails collection.
+        attachment_uri = attachment_uri.replace("videos", "thumbnails")
+
+        try:
+            print("uploading thumbnail for record id", video["id"])
+            thumbnail, _ = client.session.request(
+                method="post",
+                endpoint=attachment_uri,
+                files=files,
+                data=json.dumps(data),
+            )
+        except KintoException as e:
+            print(filename, "error during upload.", e)
+
+        # Update the video's thumbnail.
+        video["thumbnail"] = thumbnail["location"]
+
+
+def update_videos(client, videos):
+    for video in videos:
+        print(
+            "Updating video", video["id"], "with thumbnail location", video["thumbnail"]
+        )
+        client.update_record(data=video)
+
+
+def main():
+    parser = cli_utils.add_parser_options(
+        description="Move the thumbnails to attachments in the `thumbnails` collection",
+        default_server=DEFAULT_SERVER,
+        default_bucket=DEFAULT_BUCKET,
+        default_collection=DEFAULT_COLLECTION,
+    )
+
+    args = parser.parse_args()
+
+    client = cli_utils.create_client_from_args(args)
+
+    collections_metadata.update_thumbnails(client)
+
+    try:
+        client.create_bucket(if_not_exists=True)
+        client.create_collection(if_not_exists=True)
+    except KintoException:
+        # Fail silently in case of 403
+        pass
+
+    print("Fetching records")
+    videos = client.get_records()
+    filtered_videos = [
+        video for video in videos if video["thumbnail"].startswith("data:")
+    ]
+    print(
+        "Found", len(filtered_videos), "videos, upload their thumbnails as attachments"
+    )
+    upload_thumbnails(client, filtered_videos)
+    print("Updating records")
+    update_videos(client, filtered_videos)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/collections_metadata.py
@@ -0,0 +1,183 @@
+"""Collections data and permissions."""
+
+import kinto_http
+import copy
+
+VIDEOS_COLLECTIONS_METADATA = {
+    "permissions": {"write": ["account:classea12admin"]},
+    "data": {
+        "sort": "-creation_date,-last_modified",
+        "schema": {
+            "type": "object",
+            "properties": {
+                "title": {
+                    "type": "string",
+                    "title": "Titre",
+                    "description": "Titre de la vid\u00e9o",
+                },
+                "duration": {
+                    "type": "number",
+                    "title": "Duration",
+                    "description": "Duration of the video in seconds",
+                },
+                "keywords": {
+                    "type": "array",
+                    "items": {
+                        "enum": [
+                            "Am\u00e9nagement classe",
+                            "Am\u00e9nagement classe - Mobilier",
+                            "Am\u00e9nagement classe - Rangement",
+                            "Tutoriel",
+                            "\u00c9valuation",
+                            "T\u00e9moignages",
+                            "T\u00e9moignages - conseils",
+                            "Fran\u00e7ais",
+                            "Fran\u00e7ais - Lecture",
+                            "Fran\u00e7ais - Production d'\u00e9crits",
+                            "Fran\u00e7ais - Oral",
+                            "Fran\u00e7ais - Po\u00e9sie",
+                            "Autonomie",
+                            "\u00c9ducation musicale",
+                            "Graphisme",
+                            "Co-\u00e9ducation",
+                        ],
+                        "type": "string",
+                    },
+                    "title": "Mots cl\u00e9s",
+                    "description": "Mots cl\u00e9s, th\u00e9matique...",
+                    "uniqueItems": True,
+                },
+                "thumbnail": {
+                    "type": "string",
+                    "title": "Thumbnail",
+                    "description": "Link to a thumbnail for this video",
+                },
+                "description": {
+                    "type": "string",
+                    "title": "Description",
+                    "description": "Description du contenu de la vid\u00e9o",
+                },
+                "creation_date": {
+                    "type": "number",
+                    "title": "Date de cr\u00e9ation",
+                    "description": "Date d'envoi de la vid\u00e9o",
+                },
+            },
+        },
+        "uiSchema": {
+            "ui:order": [
+                "title",
+                "keywords",
+                "description",
+                "duration",
+                "thumbnail",
+                "creation_date",
+            ]
+        },
+        "attachment": {"enabled": True, "required": False},
+        "cache_expires": 0,
+        "displayFields": [
+            "title",
+            "keywords",
+            "description",
+            "duration",
+            "creation_date",
+        ],
+    },
+}
+
+CONTACTS_METADATA = {
+    "permissions": {
+        "read": ["account:classea12admin"],
+        "record:create": ["system.Authenticated", "account:classea12admin"],
+        "write": ["account:classea12admin"],
+    },
+    "data": {
+        "sort": "name",
+        "schema": {
+            "type": "object",
+            "properties": {
+                "name": {
+                    "type": "string",
+                    "title": "Name",
+                    "description": "Name of the contact",
+                },
+                "role": {
+                    "enum": ["CP", "CE1", "Formateur"],
+                    "type": "string",
+                    "title": "Role",
+                    "description": "Role of the contact",
+                },
+                "email": {
+                    "type": "string",
+                    "title": "Email",
+                    "description": "Email of the contact",
+                },
+            },
+        },
+        "uiSchema": {"ui:order": ["name", "email", "role"]},
+        "attachment": {"enabled": False, "required": False},
+        "cache_expires": 0,
+        "displayFields": ["name", "email", "role"],
+        "id": "contacts",
+    },
+}
+
+THUMBNAILS_METADATA = {
+    "permissions": {
+        "write": ["account:classea12admin"],
+        "record:create": ["system.Authenticated"],
+    },
+    "data": {
+        "sort": "-last_modified",
+        "schema": {
+            "type": "object",
+            "properties": {
+                "for": {
+                    "type": "string",
+                    "title": "For",
+                    "description": "Record ID de la video pour cette miniature",
+                }
+            },
+        },
+        "uiSchema": {"ui:order": ["for"]},
+        "attachment": {"enabled": True, "required": False},
+        "cache_expires": 0,
+        "displayFields": ["for"],
+        "id": "thumbnails",
+    },
+}
+
+
+def update_upcoming(client):
+    data = copy.deepcopy(VIDEOS_COLLECTIONS_METADATA["data"])
+    data["id"] = "upcoming"
+    permissions = copy.deepcopy(VIDEOS_COLLECTIONS_METADATA["permissions"])
+    permissions["record:create"] = ["system.Authenticated"]
+
+    client.update_collection(bucket="classea12", data=data, permissions=permissions)
+
+
+def update_videos(client):
+    data = copy.deepcopy(VIDEOS_COLLECTIONS_METADATA["data"])
+    data["id"] = "videos"
+    permissions = copy.deepcopy(VIDEOS_COLLECTIONS_METADATA["permissions"])
+    permissions["read"] = ["system.Everyone"]
+
+    client.update_collection(bucket="classea12", data=data, permissions=permissions)
+
+
+def update_contacts(client):
+    client.update_collection(
+        bucket="classea12",
+        data=CONTACTS_METADATA["data"],
+        permissions=CONTACTS_METADATA["permissions"],
+    )
+
+
+def update_thumbnails(client):
+    client.update_collection(
+        bucket="classea12",
+        data=THUMBNAILS_METADATA["data"],
+        permissions=THUMBNAILS_METADATA["permissions"],
+    )
