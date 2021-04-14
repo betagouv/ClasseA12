e3f10f574587ed0bc1de7cd977ca4477f519c757
Mathieu Agopian <mathieu@agopian.info>
2021-04-14 20:56:12 +0200

    Change the contact email address

--- src/Views/Page.elm
+++ src/Views/Page.elm
@@ -306,7 +306,7 @@ menuNodes { activePage } =
         , nav []
             [ linkMaybeActive About Route.About "À propos"
             , linkMaybeActive Participate Route.Participate "Je participe"
-            , a [ href "mailto:nicolas.leyri@beta.gouv.fr" ]
+            , a [ href "mailto:devoirsfaits.lesite@gmail.com" ]
                 [ img [ src "%PUBLIC_URL%/images/icons/32x32/message_32_white.svg" ] []
                 , text "Contactez-nous"
                 ]
