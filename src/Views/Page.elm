c21886381b0989022744d7d1deea196dca6653f5
Mathieu Agopian <mathieu@agopian.info>
2021-04-21 15:23:10 +0200

    Add the "label devoirs faits"

--- src/Views/Page.elm
+++ src/Views/Page.elm
@@ -317,6 +317,11 @@ menuNodes { activePage } =
                 , text "Inscrivez-vous à notre infolettre"
                 ]
             ]
+        , img
+            [ class "label-devoirs-faits"
+            , src "%PUBLIC_URL%/images/logos/label-devoirs-faits.svg"
+            ]
+            []
         , a
             [ class "rf-footer__brand-link"
             , href "https://www.education.gouv.fr/110-bis-le-lab-d-innovation-de-l-education-nationale-100157"
