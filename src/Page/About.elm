78bc8ae5d3a79ed96d141b31ffbd1c3999998c77
Mathieu Agopian <mathieu@agopian.info>
2021-04-21 16:02:25 +0200

    Update the About page

--- src/Page/About.elm
+++ src/Page/About.elm
@@ -43,40 +43,57 @@ view _ { title } =
                     , H.div [ HA.class "richtext" ]
                         [ H.h3 [ HA.class "title is-3" ] [ H.text "Un nouveau service, pour quoi faire ?" ]
                         , H.em [] [ H.text "Partager pour apprendre et progresser en équipe" ]
-                        , H.p [] [ H.text "Ce projet en cours de développement, co-construit avec les enseignants de collège, des CPE, des AED et des chefs d’établissement,  a pour objectif de valoriser les retours d’expérience et les échanges de pratiques personnalisées, via un produit numérique de type réseau social." ]
-                        , H.p [] [ H.text "Que vous soyez novice ou expérimenté, nous vous proposons une plateforme vidéo permettant de partager vos expériences pédagogiques auprès de vos collègues, facilement et dans le respect des pratiques de chacun." ]
+                        , H.p [] [ H.text "Ce projet en cours de développement, co-construit avec les enseignants de collège, des CPE, des AED et des chefs d’établissement, a pour objectif de faciliter le partage de ressources et de pratiques mises en œuvre dans le cadre du dispositif “Devoirs faits” au collège." ]
+                        , H.p [] [ H.text "Vous êtes un acteur de terrain engagé dans le dispositif ? Que vous soyez novice ou expérimenté, nous vous proposons une plateforme vidéo permettant de partager vos initiatives, d’illustrer vos actions et d’inspirer ainsi vos collègues partout en France, facilement et dans le respect des pratiques de chacun." ]
                         ]
                     ]
                 , H.div [ HA.class "container" ]
                     [ H.div [ HA.class "article__image" ]
                         [ H.img
-                            [ HA.src "%PUBLIC_URL%/images/photos/Nicolas_smartphone.jpg"
-                            , HA.alt "Photo de Nicolas Leyri en train de se filmer sur son smartphone"
+                            [ HA.src "%PUBLIC_URL%/images/photos/Camille_post-its.jpg"
+                            , HA.alt "Photo de Camille Martinelli plaçant des post-its"
                             ]
                             []
                         ]
                     , H.div [ HA.class "richtext" ]
                         [ H.h3 [ HA.class "title is-3" ] [ H.text "Comment participer ?" ]
                         , H.em [] [ H.text "Venez comme vous êtes, avec votre envie et vos idées !" ]
-                        , H.p [] [ H.text "Vous avez envie d'échanger et de partager des outils, des pratiques, des gestes professionnels avec vos collègues ? Rien de plus simple avec le site Devoirs faits ! Postez une courte vidéo de 1 à 2 mn en format paysage et c'est parti !" ]
+                        , H.p [] [ H.text "Vous avez envie d'échanger et de partager des outils, des pratiques, des gestes professionnels avec vos collègues ? Rien de plus simple avec le site Devoirs Faits : la communauté ! Postez une courte vidéo de 1 à 2 mn en format paysage et c'est parti !" ]
                         , H.p []
-                            [ H.text "N'oubliez pas de remplir et de nous envoyer un formulaire de droit à l'image pour vos élèves, les parents ou vous-mêmes, selon la situation, que vous pouvez "
-                            , H.a [ HA.href "http://eduscol.education.fr/internet-responsable/ressources/boite-a-outils.html" ] [ H.text "trouver ici." ]
+                            [ H.text "N'oubliez pas de remplir et de nous envoyer un formulaire de droit à l'image pour vos élèves, les parents ou vous-mêmes, selon la situation, que vous pouvez trouver ici : "
+                            , H.a [ HA.href "http://devoirs-faits.beta.gouv.fr/documents/Autorisation-captation-image-majeur_2017.pdf" ] [ H.text "autorisation adulte" ]
+                            , H.text " - "
+                            , H.a [ HA.href "http://devoirs-faits.beta.gouv.fr/documents/Autorisation-captation-image-mineur_2017.pdf" ] [ H.text "autorisation mineur" ]
                             ]
                         ]
                     ]
                 , H.div [ HA.class "container" ]
                     [ H.div [ HA.class "article__image" ]
                         [ H.img
-                            [ HA.src "%PUBLIC_URL%/images/photos/Casier_rangement_feutres.jpg"
-                            , HA.alt "Photo d'un casier de rangements de feutres de couleur"
+                            [ HA.src "%PUBLIC_URL%/images/photos/eleve_visio.jpg"
+                            , HA.alt "Deux élèves en visio"
                             ]
                             []
                         ]
                     , H.div [ HA.class "richtext" ]
                         [ H.h3 [ HA.class "title is-3" ] [ H.text "Quel sujet traiter dans ma vidéo ?" ]
                         , H.em [] [ H.text "Toute contribution est précieuse, mutualisons nos expériences et nos intelligences !" ]
-                        , H.p [] [ H.text "Avec ou sans élève, racontez votre mise en oeuvre du dispositif, décrivez son aménagement, partagez votre organisation avec vos collègues, montrez une séance en groupes...tout est permis ! Regardez les exemples déjà proposés sur le site ! Et... si le sujet a déjà été traité, n'hésitez pas à donner votre propre point de vue ! Nous nous enrichirons mutuellement." ]
+                        , H.p [] [ H.text "Avec ou sans élève, racontez votre mise en œuvre du dispositif, décrivez son aménagement, partagez votre organisation avec vos collègues, montrez une séance en groupes...tout est permis ! Regardez les exemples déjà proposés sur le site ! Et... si le sujet a déjà été traité, n'hésitez pas à donner votre propre point de vue ! Nous nous enrichirons mutuellement." ]
+                        ]
+                    ]
+                , H.div [ HA.class "richtext" ]
+                    [ H.h3 [ HA.class "title is-3" ] [ H.text "Devoirs Faits, un projet en construction" ]
+                    , H.em [] [ H.text "Un service en cours d’élaboration pour s’adapter à vos attentes !" ]
+                    , H.p [] [ H.text "Le présent prototype de site est une version bêta : cela veut dire qu’il continuera à évoluer en fonction des retours des usagers pour répondre au mieux à leurs besoins." ]
+                    , H.p []
+                        [ H.text "Ce projet est accompagné par le 110 bis, lab d’innovation de l’Education nationale, et porté par une intrapreneure issue de l’académie de Créteil. Accédez à "
+                        , H.a [ HA.href "https://www.education.gouv.fr/presentation-du-110-bis-lab-d-innovation-de-l-education-nationale-11756" ] [ H.text "cet article de présentation" ]
+                        , H.text " pour en savoir plus."
+                        ]
+                    , H.p []
+                        [ H.text "Ce prototype s’inscrit dans le dispositif "
+                        , H.a [ HA.href "https://www.education.gouv.fr/devoirs-faits-12962" ] [ H.text "Devoirs Faits" ]
+                        , H.text " porté par le ministère de l’Education nationale, de la Jeunesse et des Sports."
                         ]
                     ]
                 ]
