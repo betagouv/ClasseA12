30f93f06d437befd8cde6bc7f2ee6a0c97d6da7a
Mathieu Agopian <mathieu@agopian.info>
2022-05-10 14:02:09 +0000

    Fix video files decoder

--- src/Data/PeerTube.elm
+++ src/Data/PeerTube.elm
@@ -208,17 +208,7 @@ videoDecoder =
         |> Pipeline.optional "originallyPublishedAt" Decode.string ""
         |> Pipeline.optional "tags" (Decode.list Decode.string) []
         |> Pipeline.optional "blacklisted" Decode.bool False
-        |> Pipeline.optional "streamingPlaylists" videoStreamingPlaylistsDecoder Nothing
-
-
-videoStreamingPlaylistsDecoder : Decode.Decoder (Maybe Files)
-videoStreamingPlaylistsDecoder =
-    Decode.list
-        (Decode.at [ "files" ] videoFilesDecoder)
-        -- Go from `List (Maybe Files)` to `List Files`
-        |> Decode.map (List.filterMap identity)
-        -- Keep the first (hypothetical) File
-        |> Decode.map List.head
+        |> Pipeline.optional "files" videoFilesDecoder Nothing
 
 
 videoFilesDecoder : Decode.Decoder (Maybe Files)
